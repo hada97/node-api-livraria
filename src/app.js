eb6f9417adfec384ceefd5789229d275ba330111
Adailson <adailson65@gmail.com>
2024-09-03 17:22:00 -0300

    add livros route

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app =express();
+
+const livros =[
+    {
+        id:1,
+        titulo: "O hobbit"
+    },
+    {
+        id: 2,
+        titulo: "O senhor dos aneis"
+    }
+]
+
+app.get("/", (req, res) =>{
+    res.status(200).send("Curso de Node.js");
+});
+
+
+app.get("/livros", (req, res) =>{
+    res.status(200).json(livros);
+});
+
+export default app;
